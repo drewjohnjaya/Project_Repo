9fc1e55ba605de2cb6f8b7cefb0fb5130becf9cf
drewjohnjaya <andrewjohnsonwijaya>
2024-04-20 15:06:15 +0700

    add validations to password

--- node-express-mongodb-example/src/api/components/users/users-validator.js
+++ node-express-mongodb-example/src/api/components/users/users-validator.js
@@ -5,7 +5,15 @@ module.exports = {
     body: {
       name: joi.string().min(1).max(100).required().label('Name'),
       email: joi.string().email().required().label('Email'),
-      password: joi.string().min(6).max(32).required().label('Password'),
+      password: joi.string().min(6).max(32).uppercase().lowercase().symbol().number().required().label('Password'),
+    },
+  },
+
+  changePassword: {
+    body: {
+      email: joi.string().email().required().label('Email'),
+      old_password: joi.string().min(6).max(32).uppercase().lowercase().symbol().number().required().label('Old_Password'),
+      new_password: joi.string().min(6).max(32).uppercase().lowercase().symbol().number().required().label('New_Password'),
     },
   },
 
